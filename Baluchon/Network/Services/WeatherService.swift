a588293f66369fa8e698815b8755163b59cac352
PierreMaddio <pmsymf@gmail.com>
2022-06-19 22:18:14 +0200

    weather: implement network layer

---
+++ Baluchon/Network/Services/WeatherService.swift
@@ -0,0 +1,36 @@
+//
+//  WeatherService.swift
+//  Baluchon
+//
+//  Created by Pierre on 19/06/2022.
+//
+
+import Foundation
+
+protocol WeatherServiceProtocol: Service {
+    func getWeather(lat: String, lon: String, completion: @escaping (Weather?) -> (Void))
+}
+
+class WeatherService: ApiService, WeatherServiceProtocol {
+    
+    func getWeather(lat: String, lon: String, completion: @escaping (Weather?) -> (Void)) {
+        let urlPathStr = Path.BaseUrl.Openweathermap.path.rawValue + "?" + Path.Params.Openweathermap.lat.rawValue + "=" + lat + "&" + Path.Params.Openweathermap.lon.rawValue + "=" + lon + "&" + Constants.appID + "=" + Constants.apiKeyOpenWeathermap
+        
+        if let url = URL(string: urlPathStr) {
+            let requestURL = self.configureRequest(api: .openWeathermap, url: url, requestType: .get)
+            let task = URLSession.shared.dataTask(with: requestURL) { data, response, error in
+                if let httpResponse = response as? HTTPURLResponse {
+                    let decoder = JSONDecoder()
+                    guard let data = data, httpResponse.statusCode == 200, let response = try? decoder.decode(Weather.self, from: data) else {
+                        // Failed
+                        completion(nil)
+                        return
+                    }
+                    // success
+                    completion(response)
+                }
+            }
+            task.resume()
+        }
+    }
+}
---
+++ Baluchon/ViewModel/WeatherViewModel.swift
@@ -0,0 +1,62 @@
+//
+//  WeatherViewModel.swift
+//  Baluchon
+//
+//  Created by Pierre on 19/06/2022.
+//
+
+import Foundation
+
+class WeatherViewModel: ObservableObject {
+    @Published var loaderIsVisible: Bool = false
+    @Published var loaderIsError: Bool = false
+    @Published var cityNameDestination: String = ""
+    @Published var cityTemperatureDestination: String = ""
+    @Published var cityWeatherDescriptionDestination: String = ""
+    @Published var cityNameOrigin: String = ""
+    @Published var cityTemperatureOrigin: String = ""
+    @Published var cityWeatherDescriptionOrigin: String = ""
+    
+    // service
+    let service: WeatherServiceProtocol?
+
+    // Inject service weather
+    init(service: Service) {
+        
+        self.service = service as? WeatherServiceProtocol
+    }
+    
+    private func fetchDataForCity(lat: String, lon: String, handler: @escaping (String,String, String)-> (Void) ) {
+        loaderIsVisible = true
+        self.service?.getWeather(lat: lat, lon: lon, completion: { weatherData in
+            if let cityName = weatherData?.name,
+               let cityTemperature = weatherData?.main?.temp,
+               let cityWeatherDescription = weatherData?.weather?[0].weatherDescription {
+                DispatchQueue.main.async {
+                    self.loaderIsVisible = false
+                    self.loaderIsError = false
+                    handler(cityName,"\(cityTemperature )", cityWeatherDescription)
+                }
+            } else {
+                self.loaderIsError = true
+                }
+        })
+    }
+    
+    func fetchDataForCityDestination(lat: String, lon: String) {
+        self.fetchDataForCity(lat: lat, lon: lon) { cityName, cityTemperature, cityWeatherDescription in
+            self.cityNameDestination = cityName
+            self.cityTemperatureDestination = cityTemperature
+            self.cityWeatherDescriptionDestination = cityWeatherDescription
+        }
+    }
+    
+    func fetchDataForCityOrigin(lat: String, lon: String) {
+        self.fetchDataForCity(lat: lat, lon: lon) { cityName, cityTemperature, cityWeatherDescription in
+            self.cityNameOrigin = cityName
+            self.cityTemperatureOrigin = cityTemperature
+            self.cityWeatherDescriptionOrigin = cityWeatherDescription
+        }
+    }
+    
+}
