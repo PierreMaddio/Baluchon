613d7212fa4cca202ac9c00f6eb67dfda911a580
PierreMaddio <pmsymf@gmail.com>
2022-06-19 19:48:56 +0200

    project: init empty project

---
+++ Baluchon/BaluchonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaluchonApp.swift
+//  Baluchon
+//
+//  Created by Pierre on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BaluchonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
